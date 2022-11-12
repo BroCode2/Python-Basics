b52df5c4a798aab5d042d9f661d670e49e740587
BroCode2 <kemal.teacher@gmail.com>
2022-11-11 23:30:42 -0700

    Lists are mutable which means elements in the lists can be changed and assigned a new value

---
+++ 01_Edabit_Challenges/chage_value_in_a_list.py
@@ -0,0 +1,6 @@
+# Elements in lists are mutable, which means they can be changed. For example, let's say we've got a list of numbers:
+# Given a list of numbers, set the value of the data stored at index 1 to 88.
+
+numbers = [1, 4, 6, 8, 0]
+
+numbers[1] = 88
