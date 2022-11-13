6ef85d0dd6f585148d449e0ba8866963706548d7
BroCode2 <kemal.teacher@gmail.com>
2022-11-13 11:54:41 -0700

    added FizzBuzz challenge t the file

--- 02_Functions/fizzbuzz_practice_with_if.py
+++ 02_Functions/fizzbuzz_practice_with_if.py
@@ -1,14 +1,14 @@
 def fizz_buzz(input):
     
-    if (input % 3 ==0) and (input % 5 ==0):
+    if (input % 3 ==0) and (input % 5 ==0): # can also be written without paranthesis if input % 3 ==0 and input % 5 ==0:
         return "FizzBuzz"
-
     if input % 3 ==0:
         return "Fizz"
-
     if input % 5 ==0:
         return "Buzz"
-    else: 
-        return input
+    return input
 
-print(fizz_buzz(4))
+print(fizz_buzz(15))
+print (fizz_buzz(6))
+print (fizz_buzz(5))
+print (fizz_buzz(8))
