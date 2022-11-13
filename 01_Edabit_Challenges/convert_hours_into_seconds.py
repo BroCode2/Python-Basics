e9c32eb593c591d3bc850f5a2a3b49e61ab68f75
BroCode2 <kemal.teacher@gmail.com>
2022-11-13 00:28:54 -0700

    convert hours into seconds hours * 60 *60

---
+++ 01_Edabit_Challenges/convert_hours_into_seconds.py
@@ -0,0 +1,7 @@
+C# onvert Hours into Seconds
+
+
+def how_many_seconds(hours):
+	return hours * 60 * 60 # first multiply the hour by minutes(60), then multiply the minutes by (60)
+	
+print(how_many_seconds(2))
